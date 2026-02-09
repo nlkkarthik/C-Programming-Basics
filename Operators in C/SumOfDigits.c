8e96dc0e766894e5e43ee63d6728f85c6d90fb12
Nidumollu Leela Krishna Karthik <nidumoluleela2008@gmail.com>
2026-02-09 19:56:02 +0530

    Add SumOfDigits program in C
    Implement a program to calculate the sum of digits of a positive integer.

--- Operators in C/SumOfDigits.c
+++ Operators in C/SumOfDigits.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int number, digit, sum = 0;
+printf("Enter a positive integer: ");
+scanf("%d", &number);
+if (number <= 0)
+{
+printf("Please enter a positive integer.\n");
+return 1;
+}
 
+while (number > 0)
+{
+digit = number % 10; // Get the last digit
+sum += digit; // Add the digit to the sum
+number = number/10; // Remove the last digit from the number
+}
+
+printf("The sum of the individual digits is: %d\n", sum);
+return 0;
+}
