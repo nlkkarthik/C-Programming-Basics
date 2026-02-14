a32e1b6c92eb32e873f3e6a8a14365688f67864d
Nidumollu Leela Krishna Karthik <nidumoluleela2008@gmail.com>
2026-02-14 10:26:45 +0530

    Add Fibonacci sequence program in C

---
+++ Statements & Expressions In C/FibinacciSequence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Sequence: ");
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", first);
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
+}
